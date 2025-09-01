4be1c940ce69cbd30e6635161fb818ea1dc3036c
devendramali12 <dmali9198@gmail.com>
2025-09-01 21:46:01 +0530

    init commit

---
+++ src/Arrays/ArrayExample3.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class ArrayExample3 {
+    public static void main(String[] args) {
+        int arr[] = {20, 10, 36, 5, 2, 45, 12};
+        System.out.println(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+
+    }
+}
---
+++ src/Arrays/ArrayExample4.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class ArrayExample4 {
+    public static void main(String[] args) {
+        double[] per;   //1--> Array Declaration
+
+
+        per = new double[5];  // 2--> Array Creation
+
+        per[0] = 56.96;
+        per[1] = 85.69;
+        per[2] = 49.36;
+        per[3] = 96.36;
+        per[4] = 78.96;
+
+
+        System.out.println(per[4]);
+
+    }
+}
